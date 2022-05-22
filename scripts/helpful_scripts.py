bc82b5ba309b71a0f8f5cd15c7e165ed2d43fb4c
mauriceweber96 <weber.maurice@icloud.com>
2022-05-22 17:36:57 +0200

    minor fixes

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,8 +3,6 @@ from brownie import (
     config, 
     accounts, 
     MockV3Aggregator, 
-    VRFCoordinatorMock, 
-    LinkToken, 
     Contract, 
     interface
 )
@@ -28,8 +26,6 @@ def get_account(index=None, id=None):
 
 contract_to_mock = {
     "eth_usd_price_feed": MockV3Aggregator,
-    "vrf_coordinator": VRFCoordinatorMock,
-    "link_token": LinkToken,
 }
 
 def get_contract(contract_name):
@@ -64,16 +60,4 @@ INITIAL_VALUE = 200000000000
 def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     account = get_account()
     MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
-    link_token = LinkToken.deploy({"from": account})
-    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deployed!")
-
-def fund_with_link(contract_address, account=None, link_token=None, amount=Web3.toWei(0.3,"ether")):
-    account = account if account else get_account()
-    link_token = link_token if link_token else get_contract("link_token")
-    tx = link_token.transfer(contract_address, amount, {"from": account})
-    # link_token_contract = interface.LinkTokenInterface(link_token.address)
-    # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
-    tx.wait(1)
-    print("Funded contract")
-    return tx
